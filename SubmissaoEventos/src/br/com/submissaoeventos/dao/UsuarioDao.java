96dc997994ed34dd2ccf843c439a3372a36fea79
gustavo martins <gustavo.marinelli@gmail.com>
2016-08-05 00:00:46 -0300

    Adicionando implementação do Dao de Usuario

---
+++ SubmissaoEventos/src/br/com/submissaoeventos/dao/UsuarioDao.java
@@ -0,0 +1,39 @@
+package br.com.submissaoeventos.dao;
+
+import java.util.List;
+
+import br.com.submissaoeventos.model.Usuario;
+
+public class UsuarioDao implements Dao<Usuario> {
+
+	@Override
+	public Usuario busca(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void adiciona(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void altera(Usuario t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Usuario> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
